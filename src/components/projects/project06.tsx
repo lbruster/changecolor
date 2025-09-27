7641182973d7edfdeefe3866b9de35d3f25b5db0
Leroy B <shoked@gmail.com>
2025-09-27 08:59:29 -0500

    project start 6

--- src/components/projects/project06.tsx
+++ src/components/projects/project06.tsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const project06 = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef<ReturnType<typeof setInterval> | null>(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
+
+    return () => {
+      if (intervalIdRef.current !== null) {
+        clearInterval(intervalIdRef.current);
+        intervalIdRef.current = null;
+      }
+    };
+  }, [isRunning]);
+
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+    console.log(startTimeRef.current);
+  }
+  function stop() {
+    setIsRunning(false);
+  }
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
+  function formatTime() {
+    let hoursnum = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutesnum = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    let secondsnum = Math.floor((elapsedTime / 1000) % 60);
+    let millisecondsnum = Math.floor((elapsedTime % 1000) / 10);
+
+    const hours = String(hoursnum).padStart(2, "0");
+    const minutes = String(minutesnum).padStart(2, "0");
+    const seconds = String(secondsnum).padStart(2, "0");
+    const milliseconds = String(millisecondsnum).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  }
+
   return (
     <>
       <section className="projects">
@@ -9,6 +55,22 @@ const project06 = () => {
           <p>Learn more about us here.</p>
         </div>
       </section>
+      <div className="stopwatch-wrapper">
+        <div className="stopwatch">
+          <div className="display">{formatTime()}</div>
+          <div>
+            <button onClick={start} className="start-button">
+              Start
+            </button>
+            <button onClick={stop} className="stop-button">
+              Stop
+            </button>
+            <button onClick={reset} className="reset-button">
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </>
     /*
     6. Temporizador con Inicio, Pausa y Reinicio
