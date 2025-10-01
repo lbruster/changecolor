440ba28a11cc0e1f22bcd35e7423a4137356bcb7
Leroy B <shoked@gmail.com>
2025-09-30 23:09:38 -0500

    project start 8

--- src/components/projects/project09.tsx
+++ src/components/projects/project09.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const About = () => {
+/*
+9. Lista de Tareas con LocalStorage
+Objetivo del ejercicio: Practicar persistencia de datos con localStorage.
+Ejercicio:
+Crea una aplicación de lista de tareas.
+• Cada tarea debe incluir un texto y un checkbox para marcarla como completada.
+• Las tareas se deben guardar en localStorage para que persistan incluso si la página se recarga.
+• Debe incluir un botón para limpiar todas las tareas completadas y actualizar el localStorag
+*/
+const project09 = () => {
   return (
     <>
       <section className="projects">
@@ -10,16 +19,7 @@ const About = () => {
         </div>
       </section>
     </>
-    /*
-9. Lista de Tareas con LocalStorage
-Objetivo del ejercicio: Practicar persistencia de datos con localStorage.
-Ejercicio:
-Crea una aplicación de lista de tareas.
-• Cada tarea debe incluir un texto y un checkbox para marcarla como completada.
-• Las tareas se deben guardar en localStorage para que persistan incluso si la página se recarga.
-• Debe incluir un botón para limpiar todas las tareas completadas y actualizar el localStorag
-    */
   );
 };
 
-export default About;
+export default project09;
